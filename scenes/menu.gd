4e1be5a16a081fa459775b35112507339c45481a
Thomas Eng <thomaseng@live.no>
2025-02-18 22:05:21 +0100

    removed unused main scene

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,19 +1,13 @@
 extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
-@export var main_scene: PackedScene
-
 @onready var menu_bar = $MenuBar
 
 func _on_host_pressed():
 	# Set up server
-	peer.create_server(25566)
+	peer.create_server(25565)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
-	
-	# Add the main scene first
-	var main = main_scene.instantiate()
-	add_child(main)
 	
 	# Then add the local player
 	_add_player(multiplayer.get_unique_id())
@@ -28,6 +22,6 @@ func _add_player(id):
 func _on_join_pressed():
 	# Set up client
 	var chosen_ip = $MenuBar/TextEdit.text
-	peer.create_client(chosen_ip, 25566)
+	peer.create_client(chosen_ip, 25565)
 	multiplayer.multiplayer_peer = peer
 	menu_bar.queue_free()
