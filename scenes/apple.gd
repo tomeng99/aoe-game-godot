167352cb1918587c739605d3faf8f999c5fa20b4
Ole Strohm <strohm99@gmail.com>
2025-02-18 21:42:41 +0000

    Points!

--- scenes/apple.gd
+++ scenes/apple.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Area2D.area_entered.connect(_on_click)
+	$Area2D.body_entered.connect(_on_click)
 
-func _on_click(area: Area2D):
-	if multiplayer.is_server():
-		var overlapper = area.get_overlapping_bodies()[0]
-		print("Eaten by ", overlapper.get_multiplayer_authority())
+func _on_click(body: Node2D):
+	if multiplayer.get_unique_id() == body.get_multiplayer_authority():
+		var player_path = "/root/Menu/Player_" + str(multiplayer.get_unique_id())
+		var player_node = get_tree().get_root().get_node_or_null(player_path)
+		player_node.get_node("Points").text = str(int(player_node.get_node("Points").text)+1)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scenes/selection_box.gd
+++ scenes/selection_box.gd
@@ -18,6 +18,8 @@ func _ready():
 		print("Successfully found spawner at:", spawner_path)
 	else:
 		push_error("MultiplayerSpawner not found at path: " + spawner_path)
+	if !is_multiplayer_authority():
+		$Points.visible = false
 
 @onready var spawner: Node
 
